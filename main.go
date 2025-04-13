299f4ac6076f9c01a8d95b5886bc83871e2995fa
Paul Clarke <paulclarke345@gmail.com>
2025-04-13 14:00:37 -0400

    Add basic timing code

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 )
 
 func augmentGraph(graph Graph, positions []Point) PosGraph {
@@ -20,7 +21,14 @@ func errexit(message string) {
 	os.Exit(1)
 }
 
+func endPhase(phaseName string, phaseStart *time.Time) {
+	phaseEnd := time.Now()
+	fmt.Printf("%s: %d ns\n", phaseName, phaseEnd.Sub(*phaseStart).Nanoseconds())
+	*phaseStart = phaseEnd
+}
+
 func main() {
+	phaseStart := time.Now()
 	drawGui := true
 	var filename string
 	filenameSet := false
@@ -41,21 +49,28 @@ func main() {
 	if !filenameSet {
 		errexit("Usage: ppa-final [-png] <input-file>")
 	}
+	endPhase("Parse command line", &phaseStart)
 
 	graph, err := buildGraphFromFile(filename)
 	if err != nil {
 		errexit(fmt.Sprintf("Error building graph: %v\n", err))
 	}
+	endPhase("Build graph", &phaseStart)
 
-	positions := ForceDirectedLayout(graph, 100, 800., 600.)
+	positions := ForceDirectedLayout(graph, 10000, 800., 600.)
+	/*
 	for node, pos := range positions {
 		fmt.Printf("Node %d: (%.2f, %.2f)\n", node, pos.X, pos.Y)
 	}
+	*/
+	endPhase("Compute layout", &phaseStart)
+
 	outGraph := augmentGraph(graph, positions)
 
 	if drawGui {
 		RenderGUI(outGraph)
 	} else {
 		RenderPNG(outGraph)
+		endPhase("Create PNG", &phaseStart)
 	}
 }
