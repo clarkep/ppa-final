fb7a965a2af890aa60aff4439f1d55f95fe4c714
Paul Clarke <paulclarke345@gmail.com>
2025-04-13 23:10:31 -0400

    Example graph generator

---
+++ example_graphs.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main () {
+	r := rand.New(rand.NewSource(99))
+	n := 100
+	for i := range n {
+		for j := i; j < n; j++ {
+			if i != j && r.Intn(2) == 1 { 
+				fmt.Printf("%d %d\n", i, j)
+			}
+		}
+	}
+}
