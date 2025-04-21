85ceccedbeddaf96d883b4503810b81e213a0b2e
Paul Clarke <paulclarke345@gmail.com>
2025-04-20 21:02:29 -0400

    Partial sugiyama implementation

--- sugiyama.go
+++ sugiyama.go
@@ -4,14 +4,83 @@ import (
 	"math/rand"
 )
 
-func SugiyamaLayout (graph Graph) []Point {
-	// random for now
-	positions := make([]Point, len(graph))
-	for i, _ := range graph {
-		positions[i] = Point{
-			X: rand.Float64() * 500,
-			Y: rand.Float64() * 500,
+func removeCycles(graph Graph) Graph {
+	// Todo
+	return graph	
+}
+
+func allTrue(vertexSet []bool) bool {
+	for i := range vertexSet {
+		if !vertexSet[i] {
+			return false
 		}
 	}
+	return true
+}
+
+func assignLevels(graph Graph) [][]int {
+	// the "longest path algorithm"
+	out := make([][]int, 1) 
+	n := len(graph)
+	U := make([]bool, n)
+	Z := make([]bool, n)
+	currentLayer := 0
+	for !allTrue(U) {
+		for i := range n {
+			// select a vertex from V \ U with all outgoing edges in Z
+			if !U[i] {
+				selected := true
+				for _, v := range graph[i] {
+					if !Z[v] {
+						selected = false
+						break
+					}
+				}
+				if selected {
+					out[currentLayer] = append(out[currentLayer], i)
+					U[i] = true
+					break
+				}
+			}
+		}
+		// here, none have been selected
+		currentLayer++
+		out = append(out, make([]int, 0))
+		// Z = Z union U
+		for i := range n {
+			if U[i] {
+				Z[i] = true
+			}
+		}
+	}
+	return out
+}
+
+func orderLevels(graph Graph, levels [][]int) [][]int {
+	// Todo
+	return levels
+}
+
+func assignCoordinates(graph Graph, orders [][]int) []Point {
+	// Todo: this is a dummy algorithm
+	out := make([]Point, len(graph))
+	for x, lvl := range orders {
+		for _, u := range lvl {
+			// just assign coordinates based on (level, order in level)
+			out[u] = Point { X: float64(len(orders) - x), Y: rand.Float64() }
+		}
+	}
+	return out
+}
+
+func SugiyamaLayout(graph Graph) []Point {
+	graph2 := removeCycles(graph)
+
+	levels := assignLevels(graph2)
+
+	orders := orderLevels(graph2, levels)
+
+	positions := assignCoordinates(graph2, orders)
+
 	return positions
 }
